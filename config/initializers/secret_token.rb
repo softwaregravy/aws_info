9e4bf242ff5fb6af83ecd8e42728c7deabb83448
John Hinnegan <john.hinnegan@gmail.com>
2013-04-24 21:19:40 -0700

    basic jruby nothing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwsInfo::Application.config.secret_token = 'ae54cfe60c6cf395c996cc0ae54355a5480814de992cb785232a031ec56351650edc11206af026779894cf82d9708e69618384a916a879dc33c35653d50f9589'
