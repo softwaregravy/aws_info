430789ffea99b24c3e19df4a304b777b500a49f4
John Hinnegan <john.hinnegan@gmail.com>
2013-04-24 22:53:55 -0700

    Upgrade configs per buildpack docs

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module AwsInfo
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # per jruby instructions: https://github.com/jruby/heroku-buildpack-jruby
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ AwsInfo::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -64,4 +65,15 @@ AwsInfo::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # To get faster asset compile times as per https://github.com/jruby/heroku-buildpack-jruby
+  config.assets.js_compressor = :closure
+
+  # per https://github.com/jruby/heroku-buildpack-jruby
+  STDOUT.sync = true
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::INFO
+  config.logger = logger
+
+  config.threadsafe!
 end
