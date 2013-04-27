ed542f2f3ff89c7088a6ecd03fb5c0e139d39769
John Hinnegan <john.hinnegan@gmail.com>
2013-04-27 16:57:46 -0700

    Working basic table

--- app/controllers/aws_instances_controller.rb
+++ app/controllers/aws_instances_controller.rb
@@ -2,5 +2,6 @@ class AwsInstancesController < ApplicationController
   def index
     @pricing = Ec2Data.pricing
     @types = Ec2Data.type_specifications
+    @columns = Ec2TypeSpecification.accessible_attributes.select(&:present?).map {|attr| [attr.humanize, attr] }
   end
 end
